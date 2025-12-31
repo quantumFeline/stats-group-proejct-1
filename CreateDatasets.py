8172aa7c03cde57e7d7e8b533cadb4b93858940f
Vero <veronika.netch@gmail.com>
2025-12-31 10:19:08 +0100

    Support command-line arguments

--- CreateDatasets.py
+++ CreateDatasets.py
@@ -2,58 +2,56 @@ from __future__ import print_function
 from BooleanNetwork import BooleanNetwork
 import random
 import sys
+import argparse
 
 RANDOM_SEED = 42
 DEFAULT_N_NODES = 5
 AS_BINARY = True # False is more concise, but is harder to understand where the resulting trajectory came from
 DEFAULT_DATASET_LENGTH = 5
 DEFAULT_TRAJECTORY_LENGTH = 5
+DEFAULT_OUTPUT_FILENAME = "output.txt"
+DEFAULT_GROUND_TRUTH_FILENAME = "group_truth_network.txt"
+DEFAULT_SYNC = False
 
 if __name__ == '__main__':
     # Creating a network in a reproducible way.
-    # Usage: python CreateDatasets.py [output_filename] [ground truth filename] [n_nodes]
+    # Usage:  python2.7 CreateDatasets.py -o output.txt -g network.txt -n 10 -d 20 -l 15 -s
+    parser = argparse.ArgumentParser(description='Create Boolean network datasets')
+    parser.add_argument('-o', '--output', default=DEFAULT_OUTPUT_FILENAME,
+                        help='Output dataset filename (default: output.txt)')
+    parser.add_argument('-g', '--ground-truth', default=DEFAULT_GROUND_TRUTH_FILENAME,
+                        help='Ground truth network filename (default: ground_truth_network.txt)')
+    parser.add_argument('-n', '--nodes', type=int, default=DEFAULT_N_NODES,
+                        help='Number of nodes (default: 5)')
+    parser.add_argument('-d', '--datapoints', type=int, default=DEFAULT_DATASET_LENGTH,
+                        help='Number of trajectories (default: 5)')
+    parser.add_argument('-l', '--length', type=int, default=DEFAULT_TRAJECTORY_LENGTH,
+                        help='Length of each trajectory (default: 5)')
+    parser.add_argument('-s', '--synchronous', action='store_true',
+                        help='Use synchronous updates (default: asynchronous)')
+
+    args = parser.parse_args()
 
     random.seed(RANDOM_SEED)
 
-    if len(sys.argv) > 3:
-        network = BooleanNetwork(sys.argv[3])
-    else:
-        network = BooleanNetwork(DEFAULT_N_NODES)
-
+    network = BooleanNetwork(args.nodes)
     network.print_network()
 
-    # Under construction!
-    # For now, they do nothing:
-    if len(sys.argv) > 2:
-        filename = sys.argv[2]
-    else:
-        filename = 'group_truth_network.txt'
-
-    network.save_network(filename)
-    # network.load_network(filename) # overwrites the current network
+    network.save_network(args.ground_truth)
+    # network.load_network(args.ground_truth) # overwrites the current network
 
     #======================================================================================================================================================
 
     # Create a dataset in a reproducible way
-
-    number_of_datapoints = DEFAULT_DATASET_LENGTH
-    length_of_one_trajectory = DEFAULT_TRAJECTORY_LENGTH
-    synchronous = False
     sampling_frequency = 1
     starting_states = [] # if left empty, the starting states will be randomized for each datapoint; uncomment the next line for always starting at 0
     # starting_states = [0] * number_of_datapoints
     # If you want to specify some of the starting states, you have to write a list of length `number_of_datapoints` of ints and Nones,
     # where an integer value is the index of the state (in reverse binary), and None is "take a random starting state"
-    dataset = network.create_dataset(number_of_datapoints, length_of_one_trajectory, synchronous, sampling_frequency, starting_states)
+    dataset = network.create_dataset(args.datapoints, args.length, args.synchronous, sampling_frequency, starting_states)
 
     #======================================================================================================================================================
 
     # Print and save the dataset
     network.print_dataset(dataset, AS_BINARY)
-
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-    else:
-        filename = 'output.txt' # default
-
-    network.save_dataset(filename, dataset) #saved in the format required for BNFinder2
+    network.save_dataset(args.output, dataset) #saved in the format required for BNFinder2
