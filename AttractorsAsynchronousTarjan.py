11c2afd65b0f329455e5b59cd3b8b8aea8d3286f
Magda Czapiewska <magdalena.czapiewska001@wp.pl>
2026-01-07 15:28:17 +0100

    AttractorsAsynchronousTarjan.py fixed

--- AttractorsAsynchronousTarjan.py
+++ AttractorsAsynchronousTarjan.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 from BooleanNetwork import BooleanNetwork
 
 def build_test_network():
     transitions = [
 
         # --------------------------------------------------
-        # f0 = ¬x0 ∨ (¬x3 ∧ ¬x5)
+        # f0 = not x0 or (not x3 and not x5)
         # parents = [x0, x3, x5]
         #
         # little-endian order:
@@ -14,17 +15,17 @@ def build_test_network():
         #   0   | 0  0  0  | 1
         #   1   | 1  0  0  | 1
         #   2   | 0  1  0  | 1
-        #   3   | 1  1  0  | 1
+        #   3   | 1  1  0  | 0
         #   4   | 0  0  1  | 1
         #   5   | 1  0  1  | 0
-        #   6   | 0  1  1  | 0
+        #   6   | 0  1  1  | 1
         #   7   | 1  1  1  | 0
         #
-        ([0, 3, 5], [1, 1, 1, 1, 1, 0, 0, 0]),
+        ([0, 3, 5], [1, 1, 1, 0, 1, 0, 1, 0]),
 
 
         # --------------------------------------------------
-        # f1 = ¬x2
+        # f1 = not x2
         # parents = [x2]
         #
         # index | x2 | f1
@@ -35,7 +36,7 @@ def build_test_network():
 
 
         # --------------------------------------------------
-        # f2 = ¬x1 ∨ ¬x5
+        # f2 = not x1 or not x5
         # parents = [x1, x5]
         #
         # index = x1*1 + x5*2
@@ -50,7 +51,7 @@ def build_test_network():
 
 
         # --------------------------------------------------
-        # f3 = x0 ∨ ¬x3
+        # f3 = x0 or not x3
         # parents = [x0, x3]
         #
         # index = x0*1 + x3*2
@@ -66,10 +67,10 @@ def build_test_network():
 
         # --------------------------------------------------
         # f4 =
-        # (¬x0 ∧ x1 ∧ x3)
-        # ∨ (¬x0 ∧ x3 ∧ ¬x5)
-        # ∨ (x1 ∧ ¬x3 ∧ ¬x5)
-        # ∨ (¬x1 ∧ ¬x3 ∧ x5)
+        # (not x0 and x1 and x3)
+        # or (not x0 and x3 and not x5)
+        # or (x1 and not x3 and not x5)
+        # or (not x1 and not x3 and x5)
         #
         # parents = [x0, x1, x3, x5]
         #
@@ -78,26 +79,26 @@ def build_test_network():
         # index | x0 x1 x3 x5 | f4
         #   0   | 0  0  0  0  | 0
         #   1   | 1  0  0  0  | 0
-        #   2   | 0  1  0  0  | 1
-        #   3   | 1  1  0  0  | 0
-        #   4   | 0  0  1  0  | 1
+        #   2   | 0  1  0  0  | 1 # x1 and not x3 and not x5
+        #   3   | 1  1  0  0  | 1 # x1 and not x3 and not x5
+        #   4   | 0  0  1  0  | 1 # not x0 and x3 and not x5
         #   5   | 1  0  1  0  | 0
-        #   6   | 0  1  1  0  | 1
+        #   6   | 0  1  1  0  | 1 # not x0 and x1 and x3
         #   7   | 1  1  1  0  | 0
-        #   8   | 0  0  0  1  | 1
-        #   9   | 1  0  0  1  | 0
+        #   8   | 0  0  0  1  | 1 # not x1 and not x3 and x5
+        #   9   | 1  0  0  1  | 1 # not x1 and not x3 and x5
         #  10   | 0  1  0  1  | 0
         #  11   | 1  1  0  1  | 0
         #  12   | 0  0  1  1  | 0
         #  13   | 1  0  1  1  | 0
-        #  14   | 0  1  1  1  | 0
+        #  14   | 0  1  1  1  | 1 # not x0 and x1 and x3
         #  15   | 1  1  1  1  | 0
         #
         ([0, 1, 3, 5],
-         [0, 0, 1, 0,
+         [0, 0, 1, 1,
           1, 0, 1, 0,
-          1, 0, 0, 0,
-          0, 0, 0, 0]),
+          1, 1, 0, 0,
+          0, 0, 1, 0]),
 
 
         # --------------------------------------------------
@@ -171,11 +172,12 @@ class AsyncAnalyzerTarjan:
         cur = self.network.int_to_binary(state_int)
         succs = []
         for i in range(self.n):
-            new_state = cur.copy()
+            new_state = cur[:]
             new_state[i] = self.network.compute_next_node_state(i, cur)
             succ = self.network.binary_to_int(new_state)
             if succ != state_int:
                 succs.append(succ)
+            #succs.append(succ)
         return succs
 
     def _strongconnect(self, state):
@@ -213,51 +215,59 @@ class AsyncAnalyzerTarjan:
     # -------------------- Printing utilities --------------------
 
     def print_summary(self):
-        print("Total states:", self.num_states)
-        print("Number of asynchronous attractors:", len(self.attractors))
+        """
+        Print a brief summary of the state space.
+        """
+        print("Total number of states:", self.num_states)
+        print("Number of attractors:", len(self.attractors))
         for i, attr in enumerate(self.attractors):
-            print(f"  Attractor {i}: size {len(attr)}")
-
-    def print_attractors_binary(self):
+            print("  Attractor {}: {} states".format(i, len(attr)))
+    
+    def print_attractors_binary(self, show_decimal=True):
+        """
+        Print all attractors in binary representation.
+        Optionally also show decimal value of each state.
+    
+        :param show_decimal: if True, print decimal state before binary
+        """
         print("\nAttractors (binary states):")
         for i, attr in enumerate(self.attractors):
-            print(f"Attractor {i}:")
-            for s in sorted(attr):
-                print("  ", self.network.int_to_binary(s))
+            print("  Attractor {}:".format(i))
+            for state in sorted(attr):
+                bin_state = self.network.int_to_binary(state)
+                if show_decimal:
+                    print("    {:2d} {}".format(state, bin_state))
+                else:
+                    print("    {}".format(bin_state))
 
     def print_state_info(self):
+        """
+        Print information for all states: binary state, is_attractor, attractor_id.
+        """
         print("\nState information:")
-        for s in range(self.num_states):
-            info = self.state_info[s]
-            print(
-                f"State {s} {self.network.int_to_binary(s)} | "
-                f"is_attractor={info['is_attractor']} | "
-                f"attractor_id={info['attractor_id']}"
-            )
+        for state in range(self.num_states):
+            info = self.state_info[state]
+            bin_state = self.network.int_to_binary(state)
+            print("State {} (binary {}): is_attractor={}, attractor_id={}".format(
+                state, bin_state, info['is_attractor'], info['attractor_id']
+            ))
 
 def test_async_tarjan():
     bn = build_test_network()
-    print("===== TEST NETWORK (TARJAN) =====")
+    print("===== TEST NETWORK (Tarjan) =====")
     bn.print_network()
 
     analyzer = AsyncAnalyzerTarjan(bn)
     state_info = analyzer.analyze()
+    
+    print("\n===== SUMMARY =====")
+    analyzer.print_summary()
 
-    print("\n===== ATTRACTORS (TARJAN) =====")
-    for i, scc in enumerate(analyzer.attractors):
-        print(f"Attractor {i}:")
-        for s in sorted(scc):
-            print(" ", s, bn.int_to_binary(s))
+    print("\n===== ATTRACTORS (Tarjan) =====")
+    analyzer.print_attractors_binary(show_decimal=True)
 
-    print("\n===== STATE CLASSIFICATION (TARJAN) =====")
-    for s in range(bn.number_of_nodes ** 2):
-        info = state_info.get(s)
-        if info:
-            print(
-                f"State {s:2d} {bn.int_to_binary(s)} | "
-                f"is_attractor={info['is_attractor']} | "
-                f"attractor_id={info['attractor_id']}"
-            )
+    print("\n===== STATE CLASSIFICATION (Tarjan) =====")
+    analyzer.print_state_info()
 
 if __name__ == "__main__":
     test_async_tarjan()
