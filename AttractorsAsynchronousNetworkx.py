08f835131feeb3a7596e99f5e7c114caefd3d552
Magda Czapiewska <magdalena.czapiewska001@wp.pl>
2026-01-07 15:11:33 +0100

    AttractorsAsynchronousNetworkx.py fixed

--- AttractorsAsynchronousNetworkx.py
+++ AttractorsAsynchronousNetworkx.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from BooleanNetwork import BooleanNetwork
 import networkx as nx
 
@@ -5,7 +6,7 @@ def build_test_network():
     transitions = [
 
         # --------------------------------------------------
-        # f0 = ¬x0 ∨ (¬x3 ∧ ¬x5)
+        # f0 = not x0 or (not x3 and not x5)
         # parents = [x0, x3, x5]
         #
         # little-endian order:
@@ -15,17 +16,17 @@ def build_test_network():
         #   0   | 0  0  0  | 1
         #   1   | 1  0  0  | 1
         #   2   | 0  1  0  | 1
-        #   3   | 1  1  0  | 1
+        #   3   | 1  1  0  | 0
         #   4   | 0  0  1  | 1
         #   5   | 1  0  1  | 0
-        #   6   | 0  1  1  | 0
+        #   6   | 0  1  1  | 1
         #   7   | 1  1  1  | 0
         #
-        ([0, 3, 5], [1, 1, 1, 1, 1, 0, 0, 0]),
+        ([0, 3, 5], [1, 1, 1, 0, 1, 0, 1, 0]),
 
 
         # --------------------------------------------------
-        # f1 = ¬x2
+        # f1 = not x2
         # parents = [x2]
         #
         # index | x2 | f1
@@ -36,7 +37,7 @@ def build_test_network():
 
 
         # --------------------------------------------------
-        # f2 = ¬x1 ∨ ¬x5
+        # f2 = not x1 or not x5
         # parents = [x1, x5]
         #
         # index = x1*1 + x5*2
@@ -51,7 +52,7 @@ def build_test_network():
 
 
         # --------------------------------------------------
-        # f3 = x0 ∨ ¬x3
+        # f3 = x0 or not x3
         # parents = [x0, x3]
         #
         # index = x0*1 + x3*2
@@ -67,10 +68,10 @@ def build_test_network():
 
         # --------------------------------------------------
         # f4 =
-        # (¬x0 ∧ x1 ∧ x3)
-        # ∨ (¬x0 ∧ x3 ∧ ¬x5)
-        # ∨ (x1 ∧ ¬x3 ∧ ¬x5)
-        # ∨ (¬x1 ∧ ¬x3 ∧ x5)
+        # (not x0 and x1 and x3)
+        # or (not x0 and x3 and not x5)
+        # or (x1 and not x3 and not x5)
+        # or (not x1 and not x3 and x5)
         #
         # parents = [x0, x1, x3, x5]
         #
@@ -79,26 +80,26 @@ def build_test_network():
         # index | x0 x1 x3 x5 | f4
         #   0   | 0  0  0  0  | 0
         #   1   | 1  0  0  0  | 0
-        #   2   | 0  1  0  0  | 1
-        #   3   | 1  1  0  0  | 0
-        #   4   | 0  0  1  0  | 1
+        #   2   | 0  1  0  0  | 1 # x1 and not x3 and not x5
+        #   3   | 1  1  0  0  | 1 # x1 and not x3 and not x5
+        #   4   | 0  0  1  0  | 1 # not x0 and x3 and not x5
         #   5   | 1  0  1  0  | 0
-        #   6   | 0  1  1  0  | 1
+        #   6   | 0  1  1  0  | 1 # not x0 and x1 and x3
         #   7   | 1  1  1  0  | 0
-        #   8   | 0  0  0  1  | 1
-        #   9   | 1  0  0  1  | 0
+        #   8   | 0  0  0  1  | 1 # not x1 and not x3 and x5
+        #   9   | 1  0  0  1  | 1 # not x1 and not x3 and x5
         #  10   | 0  1  0  1  | 0
         #  11   | 1  1  0  1  | 0
         #  12   | 0  0  1  1  | 0
         #  13   | 1  0  1  1  | 0
-        #  14   | 0  1  1  1  | 0
+        #  14   | 0  1  1  1  | 1 # not x0 and x1 and x3
         #  15   | 1  1  1  1  | 0
         #
         ([0, 1, 3, 5],
-         [0, 0, 1, 0,
+         [0, 0, 1, 1,
           1, 0, 1, 0,
-          1, 0, 0, 0,
-          0, 0, 0, 0]),
+          1, 1, 0, 0,
+          0, 0, 1, 0]),
 
 
         # --------------------------------------------------
@@ -166,7 +167,7 @@ class AsyncAnalyzerNX:
     #    cur = self.network.int_to_binary(state_int)
     #    succs = []
     #    for i in range(self.n):
-    #        new_state = cur.copy()
+    #        new_state = cur[:]
     #        new_state[i] = self.network.compute_next_node_state(i, cur)
     #        succ = self.network.binary_to_int(new_state)
     #        if succ != state_int:
@@ -181,7 +182,7 @@ class AsyncAnalyzerNX:
         cur = self.network.int_to_binary(state_int)
         succs = []
         for i in range(self.n):
-            new_state = cur.copy()
+            new_state = cur[:]
             new_state[i] = self.network.compute_next_node_state(i, cur)
             succ = self.network.binary_to_int(new_state)
             if succ != state_int:
@@ -197,17 +198,24 @@ class AsyncAnalyzerNX:
         print("Total number of states:", self.num_states)
         print("Number of attractors:", len(self.attractors))
         for i, attr in enumerate(self.attractors):
-            print(f"  Attractor {i}: {len(attr)} states")
-
-    def print_attractors_binary(self):
+            print("  Attractor {}: {} states".format(i, len(attr)))
+    
+    def print_attractors_binary(self, show_decimal=True):
         """
         Print all attractors in binary representation.
+        Optionally also show decimal value of each state.
+    
+        :param show_decimal: if True, print decimal state before binary
         """
         print("\nAttractors (binary states):")
         for i, attr in enumerate(self.attractors):
-            print(f"  Attractor {i}:")
+            print("  Attractor {}:".format(i))
             for state in sorted(attr):
-                print("    ", self.network.int_to_binary(state))
+                bin_state = self.network.int_to_binary(state)
+                if show_decimal:
+                    print("    {:2d} {}".format(state, bin_state))
+                else:
+                    print("    {}".format(bin_state))
 
     def print_state_info(self):
         """
@@ -217,7 +225,10 @@ class AsyncAnalyzerNX:
         for state in range(self.num_states):
             info = self.state_info[state]
             bin_state = self.network.int_to_binary(state)
-            print(f"State {state} (binary {bin_state}): is_attractor={info['is_attractor']}, attractor_id={info['attractor_id']}")
+            print("State {} (binary {}): is_attractor={}, attractor_id={}".format(
+                state, bin_state, info['is_attractor'], info['attractor_id']
+            ))
+
 
 def test_async_nx():
     bn = build_test_network()
@@ -226,22 +237,15 @@ def test_async_nx():
 
     analyzer = AsyncAnalyzerNX(bn)
     state_info = analyzer.analyze()
+    
+    print("\n===== SUMMARY =====")
+    analyzer.print_summary()
 
     print("\n===== ATTRACTORS (NX) =====")
-    for i, scc in enumerate(analyzer.attractors):
-        print(f"Attractor {i}:")
-        for s in sorted(scc):
-            print(" ", s, bn.int_to_binary(s))
+    analyzer.print_attractors_binary(show_decimal=True)
 
     print("\n===== STATE CLASSIFICATION (NX) =====")
-    for s in range(bn.number_of_nodes ** 2):
-        info = state_info.get(s)
-        if info:
-            print(
-                f"State {s:2d} {bn.int_to_binary(s)} | "
-                f"is_attractor={info['is_attractor']} | "
-                f"attractor_id={info['attractor_id']}"
-            )
+    analyzer.print_state_info()
 
 if __name__ == "__main__":
     test_async_nx()
